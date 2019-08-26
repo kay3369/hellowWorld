15ad57d010dcd403f2324d53eb2a6e4095b09a5f
yj-605-07 <yj-605-07@DESKTOP-3PP1TD5>
2019-08-26 10:31:46 +0900

    helloworld korea

--- hellowworld1/src/hellowworld1/hellow.java
+++ hellowworld1/src/hellowworld1/hellow.java
@@ -5,7 +5,7 @@ public class hellow {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hellow");
-System.out.println("hellow!!!!!!");
+System.out.print("헬로월드");
 	}
 
 }
